e3dde9511b0d681305ffddd34813cfe45cddacf9
gzucchini <zucchinigiorgio@hotmail.it>
2020-04-27 11:25:37 +0200

    Initial commit

---
+++ app/accordion-item/accordion-item.component.ts
@@ -0,0 +1,42 @@
+import { Component, Input, Output, HostBinding, EventEmitter, OnInit } from '@angular/core';
+import { animation, trigger, state, transition, animate, style, group, query, useAnimation, stagger } from '@angular/animations';
+
+@Component({
+  selector: 'app-accordion-item',
+  templateUrl: './accordion-item.component.html',
+  styleUrls: ['./accordion-item.component.css'],
+  animations: [
+    trigger('accordionItemContentAnimation',[
+      state('isOpen', style({height: '*'})),
+      state('isClose', style({height: 0})),
+      transition('isOpen <=> isClose', group([animate('500ms')])),      
+    ])
+  ]
+})
+export class AccordionItemComponent implements OnInit {  
+  public state: string = 'isClose';
+  private _isOpen: boolean = false;
+  
+  @Input('isOpen')
+  set isOpen(value: boolean) {
+    this._isOpen = value;
+    this.state = this._isOpen ? 'isOpen' : 'isClose';
+  }
+  get isOpen(): boolean {
+    return this._isOpen;
+  }
+  @Input() title: string;
+  @Output() onToggle: EventEmitter<any> = new EventEmitter<any>();
+
+  constructor() { }
+
+
+  ngOnInit() {
+  }
+
+
+  public onClickHeader() {
+    this.isOpen = this.isOpen? false : true;
+    this.onToggle.emit(this.isOpen);
+  }
+}
